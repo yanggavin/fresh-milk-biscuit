a0a907e80994140c4df86c07625d208f4cfcad9e
Gavin Yang <gavin@live.cn>
2023-09-21 22:07:38 +0800

    initial checkin with generated code

---
+++ fresh-milk-biscuit/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fresh-milk-biscuit',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
